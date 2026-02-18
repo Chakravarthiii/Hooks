2aabeaf6689eb0c790ef2557e21df1285793a87d
Chakravarthi Saravanan <chakravarthisaravanan@Chakravarthis-MacBook-Air.local>
2026-02-18 21:59:59 +0530

    Fixed component structure and removed misplaced file

--- EP4-Comp/src/components/CounterApp.jsx
+++ EP4-Comp/src/components/CounterApp.jsx
@@ -1,49 +1,102 @@
 import React, { useState } from "react";
+import styled, { isStyledComponent } from "styled-components";
+
+const Button = styled.button`
+  width: 150px;
+  height: 50px;
+  background-color: blue;
+  color: white;
+`;
 
 const CounterApp = () => {
-  //   let count = 0;
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState("Success");
 
-  let [count, setCount] = useState(0);
-
-  function returnState() {
-    return 100;
-  }
-
-  let [sample, setSample] = useState(returnState);
+  let message;
+  let remainingClick = 10 - count;
 
   function handleIncrease() {
     setCount((prevCount) => {
       return prevCount + 1;
     });
-    setCount((prevCount) => {
-      return prevCount + 1;
-    });
-    setCount((prevCount) => {
-      return prevCount + 1;
-    });
-    // count += 1;
-    // console.log(count);
   }
-  function handleDecrease() {
-    setCount((prevCount) => {
-      return prevCount - 1;
-    });
-    setCount((prevCount) => {
-      return prevCount - 1;
-    });
-    setCount((prevCount) => {
-      return prevCount - 1;
-    });
-    // count -= 1;
-    // console.log(count);
+
+  if (count < 10) {
+    message = (
+      <div>
+        <h3> You clicked {count} times </h3>
+        <p> Still{remainingClick} more times to reach a 10% discount </p>
+      </div>
+    );
+  } else if (count === 10) {
+    message = (
+      <div>
+        <h3> You clicked {count} times </h3>
+        <p> You unlocked a 10% discount </p>
+      </div>
+    );
+  } else if (count < 20) {
+    message = (
+      <div>
+        <h3> You clicked {count} times </h3>
+        <p>
+          You're on your way for more rewards! Keep clicking for 20% discount
+        </p>
+      </div>
+    );
+  } else {
+    count === 20;
+    message = (
+      <div>
+        <h3> You clicked {count} times </h3>
+        <p>You've got the utmost discount.You're a click master</p>
+      </div>
+    );
   }
+
+  let displayComp = () => {
+    switch (data) {
+      case "Loading":
+        return <LoadingComp />;
+        break;
+      case "Success":
+        return <SuccessComp />;
+        break;
+      case "Error":
+        return <ErrorComp />;
+        break;
+    }
+  };
 
   return (
     <div>
-      <h1>click to unlock rewards 💥 </h1>
-      <button onClick={handleIncrease}> Increase </button>
+      <h1> Click to unlock rewards 💥 </h1>
+      <Button onClick={handleIncrease}> Click me! </Button>
+      {message}
+
+      {displayComp()}
+
+      {/* {count >= 10 ? (
+        <p> You unlocked a 10% discount</p>
+      ) : (
+        <p> click 10 times to unlock a reward</p>
+      )}
+
+      {count >= 20 && <p> You're a Click master </p>} */}
     </div>
   );
 };
 
 export default CounterApp;
+
+function LoadingComp() {
+  return <h6>Loading...</h6>;
+}
+
+function SuccessComp() {
+  return <h6> Success ❤️ </h6>;
+}
+
+function ErrorComp() {
+  return <h6> Error 😭 </h6>;
+}
